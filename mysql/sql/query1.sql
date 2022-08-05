15ddb7a9bfe1c5a7bdede480ffd1937b550203c3
pinetreelch <chanho.lee0215@gmail.com>
2022-08-05 17:29:03 +0900

    데이터넣기 purchase제외 추가 완료

--- mysql/sql/query1.sql
+++ mysql/sql/query1.sql
@@ -58,24 +58,22 @@ CREATE TABLE IF NOT EXISTS `tiger`.`code` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 ;
-SELECT * FROM tradBook;
-UPDATE tradBook SET tdbkIntro = 
-"
-
-"
-WHERE tdbkSeq =12;
+SELECT * FROM tradAuthor;
+UPDATE tradAuthor SET tdeducationBg = 
+null
+WHERE tdauSeq =1;
 
 SELECT * FROM tradBook;
-UPDATE tradBook SET tdbkPusblisheropinion = 
+UPDATE tradAuthor SET tdauIntro = 
 "
-
-
+베르나르 베르베르 Bernard Werber
+베르베르는 일곱 살 때부터 단편소설을 쓰기 시작한 타고난 글쟁이다. 1961년 프랑스 툴루즈에서 태어나 법학을 전공하고 국립 언론 학교에서 저널리즘을 공부했다. 저널리스트로 활동하면서 과학 잡지에 개미에 관한 글을 발표해 오다가, 드디어 1991년 120여 차례 개작을 거친 『개미』를 출간, 전 세계 독자들을 사로잡으며 단숨에 <프랑스의 천재 작가>로 떠올랐다. 이후 죽음과 삶을 넘나드는 영계 탐사단을 소재로 한 『타나토노트』, 독특한 개성으로 세계를 빚어내는 신들의 이야기 『신』, 제2의 지구를 찾아 떠난 인류의 모험 『파피용』, 고양이가 화자가 되어 인간을 상대화하는 『고양이』, 새로운 시각과 기발한 상상력이 빛나는 단편집 『나무』, 사고를 전복시키는 놀라운 지식의 향연 『상대적이며 절대적인 지식의 백과사전』 등 수많은 베스트셀러를 써냈다. 그의 작품은 전 세계 35개 언어로 번역되었으며, 2천3백만 부 이상 판매되었다.
 "
-WHERE tdbkSeq =12;
+WHERE tdauSeq =19;
 
 SELECT * FROM tradBook;
-UPDATE tradBook SET tdbkToc = 
+UPDATE tradAuthor SET tdbkToc = 
 "
 
 "
-WHERE tdbkSeq =12;
+WHERE tdauSeq =19;
